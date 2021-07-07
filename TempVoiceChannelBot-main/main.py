6b2d7e4596b6dd0ef5475fa3b38b661ea7e3b724
maxwoollons <50788222+maxwoollons@users.noreply.github.com>
2021-07-07 22:47:20 +1000

    update 0.1

---
+++ TempVoiceChannelBot-main/main.py
@@ -0,0 +1,74 @@
+from inspect import EndOfBlock
+import discord
+from discord.channel import VoiceChannel
+from discord.client import Client
+from discord.ext import commands
+from discord 
+
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+#new channel on join
+@client.event
+async def on_voice_state_update(member, before, after):
+    print("aaa")
+    guild = client.get_guild(803394988187582464)
+    print(member,before,after)
+    if after.channel and after.channel.id == 862285957699993600: #2
+        category_channel = guild.get_channel(819838582767484998)
+        channel = await guild.create_voice_channel(member.name + "" + "'s Channel", overwrites=None, category=category_channel, reason=None)
+        voice_channel = client.get_channel(channel.id)    
+        await member.move_to(voice_channel)
+        return
+        
+    else:
+        return
+
+
+#delete on 0 users
+@client.event
+async def on_voice_state_update(member, before, after): #delete channel if it is empty
+    if before.channel == None:
+        print("none")
+        return
+
+    else:
+        #print("a")
+        channel = client.get_channel(before.channel)
+        bmember_count = len(before.channel.voice_states)
+        #print(before.channel.voice_states)
+        #print(bmember_count)#before
+        if bmember_count == 0 and before.channel.id != 862285957699993600:#1
+            print("Delete")
+            await before.channel.delete()
+            return
+
+        else:
+            print("not empty yet")
+            return
+
+
+
+
+    
+
+
+
+
+
+client.run('ODI2MDAyNzY0OTk1MTAwNzMy.YGGJBQ.DrTZuA9nSbskRgL2rAwQWlBkKfU')
