17ead2492c8de21b3348b9cb635431088f75a068
maxwoollons <50788222+maxwoollons@users.noreply.github.com>
2021-07-08 07:45:23 +1000

    Working joining

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import discord
 from discord.channel import VoiceChannel
 from discord.client import Client
 from discord.ext import commands
-from discord 
 
 
 
 
-client = discord.Client()
+
+client = commands.Bot(command_prefix = "!")
+
+
+#cogs
+cogs = ['cogs.voicestate']
+
+for cog in cogs:
+    try:
+        client.load_extension(cog)
+    except Exception as e:
+        print(f'could not load cog {cog}: {str(e)}')
+
+
+#-----------------------------------------------
+
+
 
 @client.event
 async def on_ready():
@@ -28,7 +43,7 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     print("aaa")
     guild = client.get_guild(803394988187582464)
-    print(member,before,after)
+    #print(member,before,after)
     if after.channel and after.channel.id == 862285957699993600: #2
         category_channel = guild.get_channel(819838582767484998)
         channel = await guild.create_voice_channel(member.name + "" + "'s Channel", overwrites=None, category=category_channel, reason=None)
@@ -38,31 +53,6 @@ async def on_voice_state_update(member, before, after):
         
     else:
         return
-
-
-#delete on 0 users
-@client.event
-async def on_voice_state_update(member, before, after): #delete channel if it is empty
-    if before.channel == None:
-        print("none")
-        return
-
-    else:
-        #print("a")
-        channel = client.get_channel(before.channel)
-        bmember_count = len(before.channel.voice_states)
-        #print(before.channel.voice_states)
-        #print(bmember_count)#before
-        if bmember_count == 0 and before.channel.id != 862285957699993600:#1
-            print("Delete")
-            await before.channel.delete()
-            return
-
-        else:
-            print("not empty yet")
-            return
-
-
 
 
     
